8b6e062d14ed9433a9b4e1ff6b14dc8ca2f10793
Altea Qoku <altea.qoku@fshnstudent.info>
2023-05-07 18:06:13 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConnectElectronics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
