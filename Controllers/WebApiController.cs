4530ad6dc6f283f2e801c61e0f4caf6d4838322c
Xhoi Loci <xhoi.loci@imb.al>
2023-06-01 23:40:12 +0200

    Fixed dashboard and added webapi methods

--- Controllers/WebApiController.cs
+++ Controllers/WebApiController.cs
@@ -1,5 +1,8 @@
 ﻿using ConnectElectronics.Data;
+using ConnectElectronics.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace ConnectElectronics.Controllers
 {
@@ -9,7 +12,12 @@ namespace ConnectElectronics.Controllers
 	public class WebApiController : ControllerBase
 	{
 		private readonly ApplicationDbContext _context;
-		public WebApiController(ApplicationDbContext context) => _context = context;
+		private readonly IUserService _userService;
+		public WebApiController(ApplicationDbContext context, IUserService service)
+		{ 
+			_context = context;
+			_userService = service;
+		}
 		
 		public IActionResult getUsers()
 		{
@@ -20,5 +28,38 @@ namespace ConnectElectronics.Controllers
 		{
 			return Ok(_context.Produkte.OrderByDescending(p => p.Sasia).Take(3));
 		}
+
+		public IActionResult getSalesInMonth()
+		{
+			var result = from Porosite in _context.Porosit
+						 group Porosite by new
+						 {
+							 Porosite.DataPorosis.Month
+						 } into final select new
+                         {
+                             Muaji = new DateTime(1, final.Key.Month, 1).ToString("MMMM"),
+                             Count = final.Count()
+                         };
+            return Ok(result);
+		}
+
+		public IActionResult getUserHighestBuy()
+		{
+
+			return Ok(_context.Porosit.Where(p => p.DataPorosis.Month == DateTime.Now.Month).OrderByDescending(p=>p.ShumaT).Take(1));
+		}
+		public async Task<IActionResult> getProductsInfo()
+		{
+			var products = await _context.Produkte.Include(p => p.marka).ToListAsync();
+            var productDtos = products.Select(p => new 
+            {
+                Id = p.Id,
+                Emri = p.Emri,
+                Marka = p.marka.Emri,
+                Cmimi = p.Cmimi,
+				Sasia = p.Sasia
+            });
+			return Ok(productDtos);
+        }
 	}
 }
