8dba605df8fa0b4fd2ec288e0bcdf7cb392385de
ANTONETA GEGAJ <antonetagegaj@gmail.com>
2023-06-02 01:32:02 +0200

    user controller

--- Models/ViewModels/UserViewModel.cs
+++ Models/ViewModels/UserViewModel.cs
@@ -1,7 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace ConnectElectronics.Models.ViewModels
+﻿namespace ConnectElectronics.Models
 {
     public class UserViewModel
     {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public string City { get; set; }
+
+        public string Role { get; set; }
     }
 }
