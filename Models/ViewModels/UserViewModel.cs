705b12c6326f7907a1b5aa1da6c784039590ffab
Altea Qoku <altea.qoku@fshnstudent.info>
2023-06-02 01:13:04 +0200

    Role Admin managment

--- Models/ViewModels/UserViewModel.cs
+++ Models/ViewModels/UserViewModel.cs
@@ -3,5 +3,14 @@ namespace ConnectElectronics.Models.ViewModels
 {
     public class UserViewModel
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public string City { get; set; }
+
+        public string Role { get; set; }
     }
 }
